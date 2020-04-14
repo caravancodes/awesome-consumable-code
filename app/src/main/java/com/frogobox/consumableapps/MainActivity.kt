0b4db1c37e6fc1a35266eb116736e5c93f5a28ab
amirisback <faisalamircs@gmail.com>
2020-04-14 22:42:49 +0700

    add: consumable library

--- app/src/main/java/com/frogobox/consumableapps/MainActivity.kt
+++ app/src/main/java/com/frogobox/consumableapps/MainActivity.kt
@@ -2,11 +2,14 @@ package com.frogobox.consumableapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frogobox.recycler.FrogoRecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
